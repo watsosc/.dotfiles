26f6352ffe9906fccb6d9be472f626081d49ae4c
Sean Watson <sean.watson@shopify.com>
2024-10-21 13:07:13 -0400

    Add aerospace and zshrc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,8 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
@@ -21,7 +24,7 @@ vim.opt.number = true
 -- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
@@ -31,7 +34,7 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+	vim.opt.clipboard = "unnamedplus"
 end)
 
 -- Enable break indent
@@ -45,7 +48,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -62,10 +65,10 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
@@ -78,10 +81,10 @@ vim.opt.scrolloff = 10
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -89,7 +92,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -101,10 +104,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -112,32 +115,31 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup ('plugins', {
-  change_detection = {
-    notify = false,
-  }
+require("lazy").setup("plugins", {
+	change_detection = {
+		notify = false,
+	},
 })
-
--- .config/nvim/lua/plugins/testing.lua
+++ .config/nvim/lua/plugins/testing.lua
@@ -1,33 +1,41 @@
 return {
-  {
-    'nvim-neotest/neotest',
-    dependencies = {
-      'nvim-neotest/nvim-nio',
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-      'antoinemadec/FixCursorHold.nvim',
-      'haydenmeade/neotest-jest',
-      'mrcjkb/neotest-haskell',
-    },
-    opts = function()
-      return {
-        adapters = {
-          require 'neotest-jest' {
-            jestCommand = 'npm test -- --watch',
-          },
-          require 'neotest-haskell' {
-            build_tools = { 'stack', 'cabal' },
-            frameworks = { 'tasty', 'hspec', 'sydtest' },
-          },
-        },
-      }
-    end,
-    keys = {
-      { '<leader>tw', "<cmd>lua require('neotest').run.run()<cr>" },
-      { '<leader>tf', "<cmd>lua require('neotest').run.run(vim.fn.expand(' % '))<cr>" },
-      { '<leader>ts', "<cmd>lua require('neotest').run.stop()<cr>" },
-      { '<leader>ta', "<cmd>lua require('neotest').run.attach()<cr>" },
-      { '<leader>to', "<cmd>lua require('neotest').output_panel.toggle({ last_run = true })<cr>" },
-    },
-  },
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			"haydenmeade/neotest-jest",
+			"mrcjkb/neotest-haskell",
+		},
+		opts = function()
+			return {
+				adapters = {
+					require("neotest-jest")({
+						jestCommand = "npm test -- --watch",
+					}),
+					require("neotest-haskell")({
+						build_tools = { "stack", "cabal" },
+						frameworks = { "tasty", "hspec", "sydtest" },
+					}),
+				},
+			}
+		end,
+		keys = {
+			{ "<leader>tw", "<cmd>lua require('neotest').run.run()<cr>", desc = "Run all tests" },
+			{ "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand(' % '))<cr>", desc = "Run this test" },
+			{ "<leader>ts", "<cmd>lua require('neotest').run.stop()<cr>", desc = "Stop running tests" },
+			{ "<leader>tl", "<cmd>lua require('neotest').run.last()<cr>", desc = "Run last test" },
+			{ "<leader>tr", "<cmd>lua require('neotest').run.repeat()<cr>", desc = "Repeat last test" },
+			{ "<leader>td", "<cmd>lua require('neotest').run.debug()<cr>", desc = "Debug test" },
+			{ "<leader>ta", "<cmd>lua require('neotest').run.attach()<cr>", desc = "Attach to test" },
+			{ "<leader>tv", "<cmd>lua require('neotest').run.visit()<cr>", desc = "Visit test" },
+			{
+				"<leader>to",
+				"<cmd>lua require('neotest').output_panel.toggle({ last_run = true })<cr>",
+				desc = "Toggle output panel",
+			},
+		},
+	},
 }
