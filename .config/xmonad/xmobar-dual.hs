d99a6a997c9029c008f3d2e10d1e8b537c48df75
Sean Watson <watsosc@protonmail.com>
2024-11-03 15:06:24 -0500

    Fix xmonad config

--- .config/xmonad/xmobar-dual.hs
+++ .config/xmonad/xmobar-dual.hs
@@ -7,38 +7,38 @@
 --
 -- If you're using dual displays with different resolutions, adjust the
 -- position argument below using the given calculation.
-Config {
-    -- Position xmobar along the top, with stalonetray in the top right.
-    -- Shrink xmobar width to ensure stalonetray and xmobar don't overlap.
-    -- stalonetrayrc-dual is configured for 12 icons, each 19px wide.
-    -- Because of the dual display setup, we statically position xmobar.
-    -- Each display is 2560px wide. Offset left (x position) by one width.
-    -- xpos = display_width = 2560
-    -- If your left display is primary, then set xpos = 0.
-    -- ypos = 0 (top)
-    -- width = display_width - (num_icons * icon_width)
-    -- width = 2560 - (12 * 19) = 2332
-    -- height = 19
-    position = Static { xpos = 0, ypos = 0, width = 2332, height = 23 },
-    font = "Fira Code Nerd Font",
-    bgColor = "#000000",
-    fgColor = "#ffffff",
-    lowerOnStart = False,
-    overrideRedirect = False,
-    -- We don't want xmobar on all desktops in a dual display setup.
-    allDesktops = False,
-    persistent = True,
-    commands = [
-        Run Weather "CYHM" ["-t","<tempC>C <skyCondition>","-L","5","-H","30","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
-        Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
-        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Wireless "wlan0" ["-t","<essid> [<qualitybar>]","-H","80","-L","30","-h","#CEFFAC","-l","#FFB6B0","-n","#FFFFCC"] 10,
-        Run Date "%a %b %_d %l:%M" "date" 10,
-        Run Com "getMasterVolume" [] "volumelevel" 10,
-        Run StdinReader
-    ],
-    sepChar = "%",
-    alignSep = "}{",
-    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%  %wlan0wi%   Vol: <fc=#b2b2ff>%volumelevel%</fc>   %CYHM%   <fc=#FFFFCC>%date%</fc>"
-}
+Config
+    { -- Position xmobar along the top, with stalonetray in the top right.
+      -- Shrink xmobar width to ensure stalonetray and xmobar don't overlap.
+      -- stalonetrayrc-dual is configured for 12 icons, each 19px wide.
+      -- Because of the dual display setup, we statically position xmobar.
+      -- Each display is 2560px wide. Offset left (x position) by one width.
+      -- xpos = display_width = 2560
+      -- If your left display is primary, then set xpos = 0.
+      -- ypos = 0 (top)
+      -- width = display_width - (num_icons * icon_width)
+      -- width = 2560 - (12 * 19) = 2332
+      -- height = 19
+      position = Static{xpos = 0, ypos = 0, width = 2332, height = 23}
+    , font = "Fira Code Nerd Font"
+    , bgColor = "#000000"
+    , fgColor = "#ffffff"
+    , lowerOnStart = False
+    , overrideRedirect = False
+    , -- We don't want xmobar on all desktops in a dual display setup.
+      allDesktops = False
+    , persistent = True
+    , commands =
+        [ Run Weather "CYHM" ["-t", "<tempC>C <skyCondition>", "-L", "5", "-H", "30", "-n", "#CEFFAC", "-h", "#FFB6B0", "-l", "#96CBFE"] 36000
+        , Run MultiCpu ["-t", "Cpu: <total0> <total1> <total2> <total3>", "-L", "30", "-H", "60", "-h", "#FFB6B0", "-l", "#CEFFAC", "-n", "#FFFFCC", "-w", "3"] 10
+        , Run Memory ["-t", "Mem: <usedratio>%", "-H", "8192", "-L", "4096", "-h", "#FFB6B0", "-l", "#CEFFAC", "-n", "#FFFFCC"] 10
+        , Run Swap ["-t", "Swap: <usedratio>%", "-H", "1024", "-L", "512", "-h", "#FFB6B0", "-l", "#CEFFAC", "-n", "#FFFFCC"] 10
+        , Run Wireless "wlan0" ["-t", "<essid> [<qualitybar>]", "-H", "80", "-L", "30", "-h", "#CEFFAC", "-l", "#FFB6B0", "-n", "#FFFFCC"] 10
+        , Run Date "%a %b %_d %l:%M" "date" 10
+        , Run Com "/home/sean/.config/xmonad/bin/getMasterVolume" [] "volumelevel" 10
+        , Run StdinReader
+        ]
+    , sepChar = "%"
+    , alignSep = "}{"
+    , template = "%StdinReader% }{ %multicpu%   %memory%   %swap%  %wlan0wi%   Vol: <fc=#b2b2ff>%volumelevel%</fc>   %CYHM%   <fc=#FFFFCC>%date%</fc>"
+    }
--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -2,25 +2,24 @@
 -- Author: Vic Fryzel
 -- https://github.com/vicfryzel/xmonad-config
 
-import System.IO
+import qualified Data.Map as M
+import Graphics.X11.ExtraTypes.XF86
 import System.Exit
+import System.IO
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
-import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spiral
 import XMonad.Layout.Tabbed
 import XMonad.Layout.ThreeColumns
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import Graphics.X11.ExtraTypes.XF86
 import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
-
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe)
 
 ------------------------------------------------------------------------
 -- Terminal
@@ -34,25 +33,23 @@ myScreensaver = "/usr/bin/xscreensaver-command -l"
 
 -- The command to take a selective screenshot, where you select
 -- what you'd like to capture on the screen.
-mySelectScreenshot = "~/.xmonad/bin/select-screenshot"
+mySelectScreenshot = "~/.config/xmonad/bin/select-screenshot"
 
 -- The command to take a fullscreen screenshot.
-myScreenshot = "~/.xmonad/bin/screenshot"
+myScreenshot = "~/.config/xmonad/bin/screenshot"
 
 -- The command to use as a launcher, to launch commands that don't have
 -- preset keybindings.
 myLauncher = "$(yeganesh -x -- -fn 'monospace-8' -nb '#000000' -nf '#FFFFFF' -sb '#7C7C7C' -sf '#CEFFAC')"
 
 -- Location of your xmobar.hs / xmobarrc
-myXmobarrc = "~/.xmonad/xmobar-dual.hs"
-
+myXmobarrc = "~/.config/xmonad/xmobar-dual.hs"
 
 ------------------------------------------------------------------------
 -- Workspaces
 -- The default number of workspaces (virtual screens) and their names.
 --
-myWorkspaces = ["1:web","2:code","3:text","4:steam","5:media"] ++ map show [6..9]
-
+myWorkspaces = ["1:web", "2:code", "3:text", "4:steam", "5:media"] ++ map show [6 .. 9]
 
 ------------------------------------------------------------------------
 -- Window rules
@@ -68,12 +65,13 @@ myWorkspaces = ["1:web","2:code","3:text","4:steam","5:media"] ++ map show [6..9
 -- To match on the WM_NAME, you can use 'title' in the same way that
 -- 'className' and 'resource' are used below.
 --
-myManageHook = composeAll
-    [ className =? "Steam"          --> doFloat
-    , className =? "Gimp"           --> doFloat
-    , className =? "stalonetray"    --> doIgnore
-    , isFullscreen --> (doF W.focusDown <+> doFullFloat)]
-
+myManageHook =
+   composeAll
+      [ className =? "Steam" --> doFloat
+      , className =? "Gimp" --> doFloat
+      , className =? "stalonetray" --> doIgnore
+      , isFullscreen --> (doF W.focusDown <+> doFullFloat)
+      ]
 
 ------------------------------------------------------------------------
 -- Layouts
@@ -85,32 +83,34 @@ myManageHook = composeAll
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = avoidStruts (
-    ThreeColMid 1 (3/100) (1/2) |||
-    Tall 1 (3/100) (1/2) |||
-    Mirror (Tall 1 (3/100) (1/2)) |||
-    tabbed shrinkText tabConfig |||
-    Full |||
-    spiral (6/7)) |||
-    noBorders (fullscreenFull Full)
-
+myLayout =
+   avoidStruts
+      ( ThreeColMid 1 (3 / 100) (1 / 2)
+         ||| Tall 1 (3 / 100) (1 / 2)
+         ||| Mirror (Tall 1 (3 / 100) (1 / 2))
+         ||| tabbed shrinkText tabConfig
+         ||| Full
+         ||| spiral (6 / 7)
+      )
+      ||| noBorders (fullscreenFull Full)
 
 ------------------------------------------------------------------------
 -- Colors and borders
 -- Currently based on the ir_black theme.
 --
-myNormalBorderColor  = "#7c7c7c"
+myNormalBorderColor = "#7c7c7c"
 myFocusedBorderColor = "#ffb6b0"
 
 -- Colors for text and backgrounds of each tab when in "Tabbed" layout.
-tabConfig = def {
-    activeBorderColor = "#7C7C7C",
-    activeTextColor = "#CEFFAC",
-    activeColor = "#000000",
-    inactiveBorderColor = "#7C7C7C",
-    inactiveTextColor = "#EEEEEE",
-    inactiveColor = "#000000"
-}
+tabConfig =
+   def
+      { activeBorderColor = "#7C7C7C"
+      , activeTextColor = "#CEFFAC"
+      , activeColor = "#000000"
+      , inactiveBorderColor = "#7C7C7C"
+      , inactiveTextColor = "#EEEEEE"
+      , inactiveColor = "#000000"
+      }
 
 -- Color of current window title in xmobar.
 xmobarTitleColor = "#FFB6B0"
@@ -120,7 +120,6 @@ xmobarCurrentWorkspaceColor = "#CEFFAC"
 
 -- Width of the window border in pixels.
 myBorderWidth = 2
-
 
 ------------------------------------------------------------------------
 -- Key bindings
@@ -132,154 +131,185 @@ myBorderWidth = 2
 --
 myModMask = mod4Mask
 
-myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
-  ----------------------------------------------------------------------
-  -- Custom key bindings
-  --
+myKeys conf@(XConfig{XMonad.modMask = modMask}) =
+   M.fromList $
+      ----------------------------------------------------------------------
+      -- Custom key bindings
+      --
 
-  -- Start a terminal.  Terminal to start is specified by myTerminal variable.
-  [ ((modMask .|. shiftMask, xK_Return),
-     spawn $ XMonad.terminal conf)
+      -- Start a terminal.  Terminal to start is specified by myTerminal variable.
+      [
+         ( (modMask .|. shiftMask, xK_Return)
+         , spawn $ XMonad.terminal conf
+         )
+      , -- Lock the screen using command specified by myScreensaver.
 
-  -- Lock the screen using command specified by myScreensaver.
-  , ((modMask .|. controlMask, xK_l),
-     spawn myScreensaver)
+         ( (modMask .|. controlMask, xK_l)
+         , spawn myScreensaver
+         )
+      , -- Spawn the launcher using command specified by myLauncher.
+        -- Use this to launch programs without a key binding.
 
-  -- Spawn the launcher using command specified by myLauncher.
-  -- Use this to launch programs without a key binding.
-  , ((modMask, xK_p),
-     spawn myLauncher)
+         ( (modMask, xK_p)
+         , spawn myLauncher
+         )
+      , -- Take a selective screenshot using the command specified by mySelectScreenshot.
 
-  -- Take a selective screenshot using the command specified by mySelectScreenshot.
-  , ((modMask .|. shiftMask, xK_p),
-     spawn mySelectScreenshot)
+         ( (modMask .|. shiftMask, xK_p)
+         , spawn mySelectScreenshot
+         )
+      , -- Take a full screenshot using the command specified by myScreenshot.
 
-  -- Take a full screenshot using the command specified by myScreenshot.
-  , ((modMask .|. controlMask .|. shiftMask, xK_p),
-     spawn myScreenshot)
+         ( (modMask .|. controlMask .|. shiftMask, xK_p)
+         , spawn myScreenshot
+         )
+      , -- Mute volume.
 
-  -- Mute volume.
-  , ((0, xF86XK_AudioMute),
-     spawn "amixer -q set Master toggle")
+         ( (0, xF86XK_AudioMute)
+         , spawn "amixer -q set Master toggle"
+         )
+      , -- Decrease volume.
 
-  -- Decrease volume.
-  , ((0, xF86XK_AudioLowerVolume),
-     spawn "amixer -q set Master 5%-")
+         ( (0, xF86XK_AudioLowerVolume)
+         , spawn "amixer -q set Master 5%-"
+         )
+      , -- Increase volume.
 
-  -- Increase volume.
-  , ((0, xF86XK_AudioRaiseVolume),
-     spawn "amixer -q set Master 5%+")
+         ( (0, xF86XK_AudioRaiseVolume)
+         , spawn "amixer -q set Master 5%+"
+         )
+      , -- Audio previous.
 
-  -- Audio previous.
-  , ((0, 0x1008FF16),
-     spawn "")
+         ( (0, 0x1008FF16)
+         , spawn ""
+         )
+      , -- Play/pause.
 
-  -- Play/pause.
-  , ((0, 0x1008FF14),
-     spawn "")
+         ( (0, 0x1008FF14)
+         , spawn ""
+         )
+      , -- Audio next.
 
-  -- Audio next.
-  , ((0, 0x1008FF17),
-     spawn "")
+         ( (0, 0x1008FF17)
+         , spawn ""
+         )
+      , --------------------------------------------------------------------
+        -- "Standard" xmonad key bindings
+        --
 
-  --------------------------------------------------------------------
-  -- "Standard" xmonad key bindings
-  --
+        -- Close focused window.
 
-  -- Close focused window.
-  , ((modMask .|. shiftMask, xK_c),
-     kill)
+         ( (modMask .|. shiftMask, xK_c)
+         , kill
+         )
+      , -- Cycle through the available layout algorithms.
 
-  -- Cycle through the available layout algorithms.
-  , ((modMask, xK_space),
-     sendMessage NextLayout)
+         ( (modMask, xK_space)
+         , sendMessage NextLayout
+         )
+      , --  Reset the layouts on the current workspace to default.
 
-  --  Reset the layouts on the current workspace to default.
-  , ((modMask .|. shiftMask, xK_space),
-     setLayout $ XMonad.layoutHook conf)
+         ( (modMask .|. shiftMask, xK_space)
+         , setLayout $ XMonad.layoutHook conf
+         )
+      , -- Resize viewed windows to the correct size.
 
-  -- Resize viewed windows to the correct size.
-  , ((modMask, xK_n),
-     refresh)
+         ( (modMask, xK_n)
+         , refresh
+         )
+      , -- Move focus to the next window.
 
-  -- Move focus to the next window.
-  , ((modMask, xK_Tab),
-     windows W.focusDown)
+         ( (modMask, xK_Tab)
+         , windows W.focusDown
+         )
+      , -- Move focus to the next window.
 
-  -- Move focus to the next window.
-  , ((modMask, xK_j),
-     windows W.focusDown)
+         ( (modMask, xK_j)
+         , windows W.focusDown
+         )
+      , -- Move focus to the previous window.
 
-  -- Move focus to the previous window.
-  , ((modMask, xK_k),
-     windows W.focusUp  )
+         ( (modMask, xK_k)
+         , windows W.focusUp
+         )
+      , -- Move focus to the master window.
 
-  -- Move focus to the master window.
-  , ((modMask, xK_m),
-     windows W.focusMaster  )
+         ( (modMask, xK_m)
+         , windows W.focusMaster
+         )
+      , -- Swap the focused window and the master window.
 
-  -- Swap the focused window and the master window.
-  , ((modMask, xK_Return),
-     windows W.swapMaster)
+         ( (modMask, xK_Return)
+         , windows W.swapMaster
+         )
+      , -- Swap the focused window with the next window.
 
-  -- Swap the focused window with the next window.
-  , ((modMask .|. shiftMask, xK_j),
-     windows W.swapDown  )
+         ( (modMask .|. shiftMask, xK_j)
+         , windows W.swapDown
+         )
+      , -- Swap the focused window with the previous window.
 
-  -- Swap the focused window with the previous window.
-  , ((modMask .|. shiftMask, xK_k),
-     windows W.swapUp    )
+         ( (modMask .|. shiftMask, xK_k)
+         , windows W.swapUp
+         )
+      , -- Shrink the master area.
 
-  -- Shrink the master area.
-  , ((modMask, xK_h),
-     sendMessage Shrink)
+         ( (modMask, xK_h)
+         , sendMessage Shrink
+         )
+      , -- Expand the master area.
 
-  -- Expand the master area.
-  , ((modMask, xK_l),
-     sendMessage Expand)
+         ( (modMask, xK_l)
+         , sendMessage Expand
+         )
+      , -- Push window back into tiling.
 
-  -- Push window back into tiling.
-  , ((modMask, xK_t),
-     withFocused $ windows . W.sink)
+         ( (modMask, xK_t)
+         , withFocused $ windows . W.sink
+         )
+      , -- Increment the number of windows in the master area.
 
-  -- Increment the number of windows in the master area.
-  , ((modMask, xK_comma),
-     sendMessage (IncMasterN 1))
+         ( (modMask, xK_comma)
+         , sendMessage (IncMasterN 1)
+         )
+      , -- Decrement the number of windows in the master area.
 
-  -- Decrement the number of windows in the master area.
-  , ((modMask, xK_period),
-     sendMessage (IncMasterN (-1)))
+         ( (modMask, xK_period)
+         , sendMessage (IncMasterN (-1))
+         )
+      , -- Toggle the status bar gap.
+        -- TODO: update this binding with avoidStruts, ((modMask, xK_b),
 
-  -- Toggle the status bar gap.
-  -- TODO: update this binding with avoidStruts, ((modMask, xK_b),
+        -- Quit xmonad.
 
-  -- Quit xmonad.
-  , ((modMask .|. shiftMask, xK_q),
-     io (exitWith ExitSuccess))
+         ( (modMask .|. shiftMask, xK_q)
+         , io (exitWith ExitSuccess)
+         )
+      , -- Open dmenu
 
-  -- Open dmenu
-  , ((modMask, xK_p),
-     spawn "dmenu_run -b -nb black")
-  
-  -- Restart xmonad.
-  , ((modMask, xK_q),
-     restart "xmonad" True)
-  ]
-  ++
+         ( (modMask, xK_p)
+         , spawn "dmenu_run -b -nb black"
+         )
+      , -- Restart xmonad.
 
-  -- mod-[1..9], Switch to workspace N
-  -- mod-shift-[1..9], Move client to workspace N
-  [((m .|. modMask, k), windows $ f i)
-      | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-      , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-  ++
-
-  -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-  -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-  [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-      | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-      , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
-
+         ( (modMask, xK_q)
+         , restart "xmonad" True
+         )
+      ]
+         ++
+         -- mod-[1..9], Switch to workspace N
+         -- mod-shift-[1..9], Move client to workspace N
+         [ ((m .|. modMask, k), windows $ f i)
+         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+         ]
+         ++
+         -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+         -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+         [ ((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0 ..]
+         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+         ]
 
 ------------------------------------------------------------------------
 -- Mouse bindings
@@ -289,23 +319,25 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 
-myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
-  [
-    -- mod-button1, Set the window to floating mode and move by dragging
-    ((modMask, button1),
-     (\w -> focus w >> mouseMoveWindow w))
+myMouseBindings (XConfig{XMonad.modMask = modMask}) =
+   M.fromList $
+      [ -- mod-button1, Set the window to floating mode and move by dragging
 
-    -- mod-button2, Raise the window to the top of the stack
-    , ((modMask, button2),
-       (\w -> focus w >> windows W.swapMaster))
+         ( (modMask, button1)
+         , (\w -> focus w >> mouseMoveWindow w)
+         )
+      , -- mod-button2, Raise the window to the top of the stack
 
-    -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modMask, button3),
-       (\w -> focus w >> mouseResizeWindow w))
+         ( (modMask, button2)
+         , (\w -> focus w >> windows W.swapMaster)
+         )
+      , -- mod-button3, Set the window to floating mode and resize by dragging
 
-    -- you may also bind events to the mouse scroll wheel (button4 and button5)
-  ]
-
+         ( (modMask, button3)
+         , (\w -> focus w >> mouseResizeWindow w)
+         )
+         -- you may also bind events to the mouse scroll wheel (button4 and button5)
+      ]
 
 ------------------------------------------------------------------------
 -- Status bars and logging
@@ -317,7 +349,6 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
 -- > logHook = dynamicLogDzen
 --
 
-
 ------------------------------------------------------------------------
 -- Startup hook
 -- Perform an arbitrary action each time xmonad starts or is restarted
@@ -327,24 +358,26 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
 -- By default, do nothing.
 myStartupHook = return ()
 
-
 ------------------------------------------------------------------------
 -- Run xmonad with all the defaults we set up.
 --
 main = do
-  xmproc <- spawnPipe ("xmobar " ++ myXmobarrc)
-  xmonad $ ewmhFullscreen . docks $ defaults { 
-      logHook = dynamicLogWithPP $ xmobarPP {
-            ppOutput = hPutStrLn xmproc
-          , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
-          , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
-          , ppSep = "   "
-      }
-      , manageHook = manageDocks <+> myManageHook
---      , startupHook = docksStartupHook <+> setWMName "LG3D"
-      , startupHook = setWMName "LG3D"
-  }
-
+   xmproc <- spawnPipe ("xmobar " ++ myXmobarrc)
+   xmonad $
+      ewmhFullscreen . docks $
+         defaults
+            { logHook =
+               dynamicLogWithPP $
+                  xmobarPP
+                     { ppOutput = hPutStrLn xmproc
+                     , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
+                     , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
+                     , ppSep = "   "
+                     }
+            , manageHook = manageDocks <+> myManageHook
+            , --      , startupHook = docksStartupHook <+> setWMName "LG3D"
+              startupHook = setWMName "LG3D"
+            }
 
 ------------------------------------------------------------------------
 -- Combine it all together
@@ -354,22 +387,21 @@ main = do
 --
 -- No need to modify this.
 --
-defaults = def {
-    -- simple stuff
-    terminal           = myTerminal,
-    focusFollowsMouse  = myFocusFollowsMouse,
-    borderWidth        = myBorderWidth,
-    modMask            = myModMask,
-    workspaces         = myWorkspaces,
-    normalBorderColor  = myNormalBorderColor,
-    focusedBorderColor = myFocusedBorderColor,
-
-    -- key bindings
-    keys               = myKeys,
-    mouseBindings      = myMouseBindings,
-
-    -- hooks, layouts
-    layoutHook         = smartBorders $ myLayout,
-    manageHook         = myManageHook,
-    startupHook        = myStartupHook
-}
+defaults =
+   def
+      { -- simple stuff
+        terminal = myTerminal
+      , focusFollowsMouse = myFocusFollowsMouse
+      , borderWidth = myBorderWidth
+      , modMask = myModMask
+      , workspaces = myWorkspaces
+      , normalBorderColor = myNormalBorderColor
+      , focusedBorderColor = myFocusedBorderColor
+      , -- key bindings
+        keys = myKeys
+      , mouseBindings = myMouseBindings
+      , -- hooks, layouts
+        layoutHook = smartBorders $ myLayout
+      , manageHook = myManageHook
+      , startupHook = myStartupHook
+      }
